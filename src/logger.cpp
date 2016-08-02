220d4cc886f7b55245614f22896f19a868185efa
Martin Kampas <martin.kampas@jolla.com>
2016-08-02 14:01:25 +0200

    Logger: Handle QtInfoMsg
    Change-Id: I6a0001fbd5a971efafbbd1a43ab7ea23ca921bab
    Reviewed-by: Juergen Bocklage-Ryannel <juergen.bocklage-ryannel@pelagicore.com>

--- src/logger.cpp
+++ src/logger.cpp
@@ -99,6 +99,9 @@ void Logger::messageHandler(QtMsgType type, const QMessageLogContext& context, c
     case QtDebugMsg:
         fprintf(stderr, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
         break;
+    case QtInfoMsg:
+        fprintf(stderr, "Info: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        break;
     case QtWarningMsg:
         fprintf(stderr, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
         break;
